eb15feb18f2281c1ae7d8f7a38bef00833f79a7c
gabduoc <gab.zepeda@duocuc.cl>
2026-04-21 18:24:05 -0400

    feat: La conexion esta verificada, es exitosa, fastaspi responde como se espera y la busqueda por categoria funciona correctamente

--- Backend/routers/search.py
+++ Backend/routers/search.py
@@ -13,18 +13,19 @@ async def busqueda_por_categoria(categoria: str):
         query = services_ref.where("categoria", "==", categoria)
 
         #3 Obtenemos los resultados de la consulta
-        results = query.stream()
+        results = []
 
-        for doc in query:
+        for doc in query.stream():
             # Es necesario convertir cada documento de la base de datos a un diccionario
             # Solo necesitamos agregar los campos relevantes para el dato a devolver al cliente
             servicio_data = doc.to_dict()
 
-            servicio_data["id"] = servicio_data.get("id")  # Agregamos el ID del documento a los datos del servicio
+            servicio_data["idServicio"] = servicio_data.get("idServicio")  # Agregamos el ID del documento a los datos del servicio
+            servicio_data["idTecnico"] = servicio_data.get("idTecnico") # Agregamos el ID del técnico a los datos del servicio
             servicio_data["titulo"] = servicio_data.get("titulo")  # Agregamos el título del servicio a los datos del servicio
             servicio_data["categoria"] = categoria  # Agregamos la categoría al resultado
             servicio_data["precio base"] = servicio_data.get("precio base")  # Agregamos el precio base del servicio a los datos del servicio
-            servicio_data["descripcion"] = servicio_data.get("descripcion")  # Agregamos la descripción del servicio a los datos del servicio
+            servicio_data["descripción"] = servicio_data.get("descripción")  # Agregamos la descripción del servicio a los datos del servicio
             servicio_data["idTecnico"] = servicio_data.get("idTecnico")  # Agregamos el ID del técnico a los datos del servicio
             
             # Guardamos el resultado en la lista de resultados a devolver al cliente
