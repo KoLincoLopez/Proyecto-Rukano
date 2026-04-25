6c34b0e8dc7cda5542a215f05344eb40363b1936
Danae17 <da.covarrubia@duocuc.cl>
2026-04-24 23:34:52 -0400

    fix: limpieza completa y gitignore aplicado

--- Backend/core/firebase_config.py
+++ Backend/core/firebase_config.py
@@ -20,7 +20,7 @@ def initialize_firebase():
     # Verificamos si la app ya fue inicializada para evitar errores de duplicidad
     if not firebase_admin._apps:
         # Obtenemos la ruta o el contenido del JSON desde la variable de entorno
-        cert_path = os.getenv("FIREBASE_KEY_PATH")
+        cert_path = os.path.join(os.path.dirname(__file__), "firebase_key.json")
         
         if cert_path:
             cred = credentials.Certificate(cert_path)
