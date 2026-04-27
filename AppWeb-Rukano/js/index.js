86310f6188b83bc702bf227da2aea125e78bfd7d
KoLincoLopez <lincopilconstanza3@gmail.com>
2026-04-26 22:53:20 -0400

    update: se agrego las media querys para la responsividad
    Se agregaron las media querys para 1024px, 768px y 480px

--- AppWeb-Rukano/js/index.js
+++ AppWeb-Rukano/js/index.js
@@ -8,4 +8,16 @@ document.addEventListener("DOMContentLoaded", () => {
             navbar.classList.remove("scrolled");
         }
     });
+
+    if (btnHamburguesa) {
+        btnHamburguesa.addEventListener("click", () => {
+            // Si la pantalla es menor a 768px, el menú ocupa el 100%, si no, 300px.
+            if (window.innerWidth <= 768) {
+                sideMenu.style.width = "100%";
+            } else {
+                sideMenu.style.width = "300px";
+            }
+            overlay.style.display = "block";
+        });
+    }
 });
