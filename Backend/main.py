733bce1a083ac4fa9d3928b649182d9f3b8977f7
gabduoc <gab.zepeda@duocuc.cl>
2026-05-01 17:13:57 -0400

    feat: Implemente la logica de los servicios y las keywords, pero aun faltan los formularios de los servicios

--- Backend/main.py
+++ Backend/main.py
@@ -16,7 +16,8 @@ app.include_router(reviews.router)
 app.include_router(search.router)
 app.include_router(citas.router, prefix="/citas")
 app.include_router(servicios.router, prefix="/servicios")
-
+app.include_router(reviews.router, prefix="/reviews")
+app.include_router(servicios.router, prefix="/servicios")
 
 @app.get("/")
 def inicio():
@@ -44,3 +45,10 @@ def validate_user(authorization: str = Header(None)):
             status_code=403,
             detail="Token inválido o usuario no reconocido"
         )
+    
+"""
+para ejecutar el servidor localmente y realizar pruebas ingresa el siguiente comando en la terminal:
+uvicorn main:app --reload 
+(si da error prueba haciendo cd a la carpeta Backend y luego ejecuta el comando o revisando la ruta del archivo llamado .env)
+Esto iniciará el servidor de desarrollo de FastAPI y podrás acceder a la documentación automática en http://localhost:8000/docs
+"""
