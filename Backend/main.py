224133ca1b1c29977e3adbbffda96f1bc8806995
KoLincoLopez <lincopilconstanza3@gmail.com>
2026-05-26 11:54:40 -0400

    update: CORS
    se agrego la Url del frontend que esa arriba en Render.

--- Backend/main.py
+++ Backend/main.py
@@ -27,6 +27,7 @@ app.add_middleware(
         "http://127.0.0.1:5501",
         "http://localhost:5500",
         "http://localhost:5501",
+        "https://proyecto-rukano.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
